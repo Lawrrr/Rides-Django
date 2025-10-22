eff58c98f2957308c6e6f5461ef7bff7fd186ccd
Lawrrr <lawrboni@gmail.com>
2025-10-22 18:29:26 +0800

    Feature: Models and Migrations

---
+++ myapp/api/models/__init__.py
@@ -0,0 +1,3 @@
+from .user import User
+from .ride import Ride
+from .ride_event import Ride_Event
---
+++ myapp/api/models/ride.py
@@ -0,0 +1,13 @@
+from django.db import models
+from .user import User
+
+class Ride(models.Model):
+    id_ride = models.IntegerField(primary_key=True)
+    status = models.CharField(max_length=100, null=False)
+    id_rider = models.ForeignKey(User, null=False, on_delete=models.CASCADE, related_name="user_rider")
+    id_driver = models.ForeignKey(User, null=False, on_delete=models.CASCADE, related_name="user_driver")
+    pickup_latitude = models.FloatField(max_length=12)
+    pickup_longitude = models.FloatField(max_length=12)
+    dropoff_latitude = models.FloatField(max_length=12)
+    dropoff_longitude = models.FloatField(max_length=12)
+

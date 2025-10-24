87cc606f3a788193dedea4e0940d56af4f6a20b0
Lawrrr <lawrboni@gmail.com>
2025-10-25 01:41:12 +0800

    Feature: Filter Last 24 hrs Ride Events List on Ride Data

--- myapp/api/views/ride_views.py
+++ myapp/api/views/ride_views.py
@@ -1,3 +1,6 @@
+from django.db.models import F, Prefetch, Window
+from django.db.models.functions import RowNumber
+
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -5,42 +8,100 @@ from rest_framework.permissions import IsAuthenticated
 
 # API app
 from api.permissions import IsAdmin
-from api.models import Ride
+from api.models import Ride, RideEvent
 from api.serializers import RideSerializer
 
 # Helper defs
 from api.helpers import annotate_distance, filter_nearby
+
+# utils
+from django.utils import timezone
+from datetime import timedelta
 
 class RideViewset(viewsets.ModelViewSet):
     queryset = Ride.objects.all()
     serializer_class = RideSerializer
     permission_classes = [IsAuthenticated, IsAdmin]
 
+    def get_queryset(self):
+        """
+        - This prevents fetching full RideEvent lists and avoids N+1 queries
+        - I've limit the RideEvent list to 10 events
+        """
+        cutoff = timezone.now() - timedelta(hours=24)
+
+        prefetch_qs = (
+            RideEvent
+            .objects
+            .filter(created_at__gte=cutoff)
+            .annotate(
+                row_number=Window(
+                    expression=RowNumber(),
+                    partition_by=[F('id_ride')],
+                    order_by=F('created_at').desc()
+                )
+            )
+            .filter(row_number__lte=10)
+            .order_by('-created_at')
+        )
+
+        return (
+            Ride.objects
+                .prefetch_related(
+                    Prefetch(
+                        'ride_events', 
+                        queryset=prefetch_qs, 
+                        to_attr='todays_ride_events'
+                    )
+                )
+        )
+
     def list(self, request, *args, **kwargs):
         """
         Default sorting is pickup_time when there is no sort query param
-        url sample: /api/rides/?sort=distance&lat=1.0&lon=14.000
+        distance url sample: /api/rides/?sort=distance&lat=1.0&lon=14.000
+        rider email url sample: /api/rides/?sort=rider-email&email=dutch@gmail.com
+        status url sample: /api/rides/?sort=ride-status&status=dropoff
         """
         sort = request.query_params.get('sort', 'pickup_time')
         lat = request.query_params.get('lat')
         lon = request.query_params.get('lon')
+        email = request.query_params.get('email')
+        rideStatus = request.query_params.get('status')
 
         qs = self.get_queryset()
 
-        if sort == 'pickup_time':
+        if sort == 'pickup-time':
             qs = qs.order_by('-pickup_time')
+        elif sort == 'rider-email':
+            if not email:
+                return Response(
+                    {"detail": "email is required for email sorting"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            qs = qs.filter(id_rider__email=email)
+        elif sort == 'ride-status':
+            if not rideStatus:
+                return Response(
+                    {"detail": "status is required for ride status sorting"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            qs = qs.filter(status=rideStatus)
         elif sort == 'distance':
             if not lat or not lon:
                 return Response(
                     {"detail": "lat and lon are required for distance sorting"},
                     status=status.HTTP_400_BAD_REQUEST,
                 )
+            
             lat, lon = float(lat), float(lon)
-
             qs = filter_nearby(qs, lat, lon)
             qs = annotate_distance(qs, lat, lon).order_by('distance')
         else:
-            return Response({"Error": "Invalid parameters"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"Error": "Invalid query parameters"}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
         
         page = self.paginate_queryset(qs)
         if page is not None:
@@ -48,32 +109,4 @@ class RideViewset(viewsets.ModelViewSet):
             return self.get_paginated_response(serializer.data)
         
         serializer = self.get_serializer(qs, many=True)
-        return Response(serializer.data)
-
-    @action(detail=False, methods=['get'])
-    def rider_email(self, request):
-        email = request.query_params.get('email')
-        
-        if not email:
-            return Response(
-                {"error": "Please provide email as query parameter"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        
-        rides = Ride.objects.filter(id_rider__email=email)
-        serializer = RideSerializer(rides, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    @action(detail=False, methods=['get'])
-    def ride_status(self, request):
-        statusParam = request.query_params.get('status')
-        
-        if not status:
-            return Response(
-                {"error": "Please provide status as query parameter"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        
-        rides = Ride.objects.filter(status=statusParam)
-        serializer = RideSerializer(rides, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / env('SQLITE_DB'),
     }
 }
 
@@ -147,8 +147,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
 
